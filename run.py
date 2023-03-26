5a7b627f4f441c5f001da541fb58f0698597a376
marko042 <marko.sormaz1211@gmail.com>
2023-03-26 17:59:08 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+
+print("Welcome to Wordle, let's play\n")
+print('Please type  a 5 letter word and hit return')
+
+def gen_rand_word()
+"""
+Generates random word by opening and reading from a file using a split delimiter function 
+previously createdcontaining a number of 5 letter words.
+"""
+    with open("words.txt", "r") as w:
+        allText = w.read()
+        words = list(map(str, allText.split()))
+  
+    
+
